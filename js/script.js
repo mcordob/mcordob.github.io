f32725889cdec2571f66a205213b03a298bccdd2
Meyel C <80123450+mcordob@users.noreply.github.com>
2023-02-28 09:22:46 -0600

    primera subida

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Cybersecurity engineer", "Discord Developer", "Web pages creator", "Database Developer", "Script Developer", "Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
